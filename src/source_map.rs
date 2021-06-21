2492c7c23f1e02598da4faf6e62f6c3f720e0734
Mário Feroldi <mferoldif@gmail.com>
2021-06-21 02:35:57 -0300

    Refactor BytePos to a named tuple

--- src/source_map.rs
+++ src/source_map.rs
@@ -1,17 +1,15 @@
 #[derive(Debug, PartialEq)]
-struct BytePos {
-    raw: usize,
-}
+struct BytePos(usize);
 
 fn parse_start_pos_of_source_lines(source_text: &str) -> Vec<BytePos> {
     std::iter::once(if source_text.is_empty() {
         None
     } else {
-        Some(BytePos { raw: 0 })
+        Some(BytePos(0))
     })
     .chain(source_text.bytes().enumerate().map(|(pos, byte)| {
         if byte == b'\n' && pos + 1 < source_text.len() {
-            Some(BytePos { raw: pos + 1 })
+            Some(BytePos(pos + 1))
         } else {
             None
         }
@@ -35,34 +33,28 @@ mod tests {
     fn parse_start_pos_of_source_lines_from_text_without_newline() {
         let source_text = "some text without newline";
         let source_line_pos = parse_start_pos_of_source_lines(source_text);
-        assert_eq!(source_line_pos, vec![BytePos { raw: 0usize }]);
+        assert_eq!(source_line_pos, vec![BytePos(0usize)]);
     }
 
     #[test]
     fn parse_start_pos_of_source_lines_from_text_with_newline_at_the_end() {
         let source_text = "abc\n";
         let source_line_pos = parse_start_pos_of_source_lines(source_text);
-        assert_eq!(source_line_pos, vec![BytePos { raw: 0usize }]);
+        assert_eq!(source_line_pos, vec![BytePos(0usize)]);
     }
 
     #[test]
     fn parse_start_pos_of_source_lines_from_text_with_newline_in_the_middle() {
         let source_text = "abc\ndef";
         let source_line_pos = parse_start_pos_of_source_lines(source_text);
-        assert_eq!(
-            source_line_pos,
-            vec![BytePos { raw: 0usize }, BytePos { raw: 4usize }]
-        );
+        assert_eq!(source_line_pos, vec![BytePos(0usize), BytePos(4usize)]);
     }
 
     #[test]
     fn parse_start_pos_of_source_lines_from_text_with_newline_at_the_start() {
         let source_text = "\nabc";
         let source_line_pos = parse_start_pos_of_source_lines(source_text);
-        assert_eq!(
-            source_line_pos,
-            vec![BytePos { raw: 0usize }, BytePos { raw: 1usize }]
-        );
+        assert_eq!(source_line_pos, vec![BytePos(0usize), BytePos(1usize)]);
     }
 
     #[test]
@@ -72,10 +64,10 @@ mod tests {
         assert_eq!(
             source_line_pos,
             vec![
-                BytePos { raw: 0usize },
-                BytePos { raw: 1usize },
-                BytePos { raw: 2usize },
-                BytePos { raw: 3usize },
+                BytePos(0usize),
+                BytePos(1usize),
+                BytePos(2usize),
+                BytePos(3usize),
             ]
         );
     }
@@ -86,11 +78,7 @@ mod tests {
         let source_line_pos = parse_start_pos_of_source_lines(source_text);
         assert_eq!(
             source_line_pos,
-            vec![
-                BytePos { raw: 0usize },
-                BytePos { raw: 4usize },
-                BytePos { raw: 5usize },
-            ]
+            vec![BytePos(0usize), BytePos(4usize), BytePos(5usize)]
         );
     }
 }
