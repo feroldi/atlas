b1c96e3b16cc3e08368a8eac9cdf3f08f6048aa2
Mário Feroldi <mferoldif@gmail.com>
2021-06-21 01:24:28 -0300

    Rename parse_source_line_byte_positions

--- src/source_map.rs
+++ src/source_map.rs
@@ -3,18 +3,18 @@ struct BytePos {
     raw: usize,
 }
 
-fn parse_source_line_positions(_: &str) -> Vec<BytePos> {
+fn parse_source_line_byte_positions(_: &str) -> Vec<BytePos> {
     Vec::new()
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{BytePos, parse_source_line_positions};
+    use super::{parse_source_line_byte_positions, BytePos};
 
     #[test]
     fn parse_source_line_positions_from_empty_text() {
         let input = "";
-        let source_line_pos = parse_source_line_positions(input);
+        let source_line_pos = parse_source_line_byte_positions(input);
         assert_eq!(source_line_pos, Vec::<BytePos>::new());
     }
 }
