0933873068eb91801bebabc7b6ac8766c0d3f367
Mário Feroldi <mferoldif@gmail.com>
2021-06-21 03:05:28 -0300

    Put tests inside a module

--- src/source_map.rs
+++ src/source_map.rs
@@ -15,65 +15,67 @@ fn parse_start_pos_of_source_lines(source_text: &str) -> Vec<BytePos> {
 
 #[cfg(test)]
 mod tests {
-    use super::{parse_start_pos_of_source_lines, BytePos};
+    mod parse_start_pos_of_source_lines {
+        use crate::source_map::{parse_start_pos_of_source_lines, BytePos};
 
-    #[test]
-    fn parse_start_pos_of_source_lines_from_empty_text() {
-        let source_text = "";
-        let source_line_pos = parse_start_pos_of_source_lines(source_text);
-        assert_eq!(source_line_pos, vec![]);
-    }
+        #[test]
+        fn empty_text() {
+            let source_text = "";
+            let source_line_pos = parse_start_pos_of_source_lines(source_text);
+            assert_eq!(source_line_pos, vec![]);
+        }
 
-    #[test]
-    fn parse_start_pos_of_source_lines_from_text_without_newline() {
-        let source_text = "some text without newline";
-        let source_line_pos = parse_start_pos_of_source_lines(source_text);
-        assert_eq!(source_line_pos, vec![BytePos(0usize)]);
-    }
+        #[test]
+        fn text_without_newline() {
+            let source_text = "some text without newline";
+            let source_line_pos = parse_start_pos_of_source_lines(source_text);
+            assert_eq!(source_line_pos, vec![BytePos(0usize)]);
+        }
 
-    #[test]
-    fn parse_start_pos_of_source_lines_from_text_with_newline_at_the_end() {
-        let source_text = "abc\n";
-        let source_line_pos = parse_start_pos_of_source_lines(source_text);
-        assert_eq!(source_line_pos, vec![BytePos(0usize)]);
-    }
+        #[test]
+        fn text_with_newline_at_the_end() {
+            let source_text = "abc\n";
+            let source_line_pos = parse_start_pos_of_source_lines(source_text);
+            assert_eq!(source_line_pos, vec![BytePos(0usize)]);
+        }
 
-    #[test]
-    fn parse_start_pos_of_source_lines_from_text_with_newline_in_the_middle() {
-        let source_text = "abc\ndef";
-        let source_line_pos = parse_start_pos_of_source_lines(source_text);
-        assert_eq!(source_line_pos, vec![BytePos(0usize), BytePos(4usize)]);
-    }
+        #[test]
+        fn text_with_newline_in_the_middle() {
+            let source_text = "abc\ndef";
+            let source_line_pos = parse_start_pos_of_source_lines(source_text);
+            assert_eq!(source_line_pos, vec![BytePos(0usize), BytePos(4usize)]);
+        }
 
-    #[test]
-    fn parse_start_pos_of_source_lines_from_text_with_newline_at_the_start() {
-        let source_text = "\nabc";
-        let source_line_pos = parse_start_pos_of_source_lines(source_text);
-        assert_eq!(source_line_pos, vec![BytePos(0usize), BytePos(1usize)]);
-    }
+        #[test]
+        fn text_with_newline_at_the_start() {
+            let source_text = "\nabc";
+            let source_line_pos = parse_start_pos_of_source_lines(source_text);
+            assert_eq!(source_line_pos, vec![BytePos(0usize), BytePos(1usize)]);
+        }
 
-    #[test]
-    fn parse_start_pos_of_source_lines_from_text_with_various_newlines_at_the_start() {
-        let source_text = "\n\n\nabc";
-        let source_line_pos = parse_start_pos_of_source_lines(source_text);
-        assert_eq!(
-            source_line_pos,
-            vec![
-                BytePos(0usize),
-                BytePos(1usize),
-                BytePos(2usize),
-                BytePos(3usize),
-            ]
-        );
-    }
+        #[test]
+        fn text_with_various_newlines_at_the_start() {
+            let source_text = "\n\n\nabc";
+            let source_line_pos = parse_start_pos_of_source_lines(source_text);
+            assert_eq!(
+                source_line_pos,
+                vec![
+                    BytePos(0usize),
+                    BytePos(1usize),
+                    BytePos(2usize),
+                    BytePos(3usize),
+                ]
+            );
+        }
 
-    #[test]
-    fn parse_start_pos_of_source_lines_from_text_with_various_newlines_at_the_end() {
-        let source_text = "abc\n\n\n";
-        let source_line_pos = parse_start_pos_of_source_lines(source_text);
-        assert_eq!(
-            source_line_pos,
-            vec![BytePos(0usize), BytePos(4usize), BytePos(5usize)]
-        );
+        #[test]
+        fn text_with_various_newlines_at_the_end() {
+            let source_text = "abc\n\n\n";
+            let source_line_pos = parse_start_pos_of_source_lines(source_text);
+            assert_eq!(
+                source_line_pos,
+                vec![BytePos(0usize), BytePos(4usize), BytePos(5usize)]
+            );
+        }
     }
 }
