223e11238517ec7822a5c8fedb710979bcd0ed76
Mário Feroldi <mferoldif@gmail.com>
2021-07-04 16:26:15 -0300

    Peek always returns the current char from input

--- src/char_bumper.rs
+++ src/char_bumper.rs
@@ -1,7 +1,7 @@
 use std::marker::PhantomData;
 
 struct CharBumper<'str> {
-    _phantom: PhantomData<&'str str>,
+    source: &'str str,
 }
 
 // TODO: peek_is
@@ -9,16 +9,14 @@ struct CharBumper<'str> {
 // TODO: bump_if
 
 impl<'a> From<&'a str> for CharBumper<'a> {
-    fn from(_: &'a str) -> CharBumper<'a> {
-        CharBumper {
-            _phantom: PhantomData,
-        }
+    fn from(value: &'a str) -> CharBumper<'a> {
+        CharBumper { source: value }
     }
 }
 
 impl CharBumper<'_> {
     fn peek(&self) -> Option<char> {
-        None
+        self.source.chars().next()
     }
 }
 
@@ -31,6 +29,21 @@ mod tests {
         fn should_return_none_when_input_is_empty() {
             let char_bumper = CharBumper::from("");
             assert_eq!(char_bumper.peek(), None::<char>);
+        }
+
+        #[test]
+        fn should_return_first_char_from_input_when_it_is_not_empty() {
+            let char_bumper = CharBumper::from("abc");
+            assert_eq!(char_bumper.peek(), Some('a'));
+        }
+
+        #[test]
+        fn multiple_calls_to_peek_should_return_the_same_char() {
+            let char_bumper = CharBumper::from("abc");
+
+            assert_eq!(char_bumper.peek(), Some('a'));
+            assert_eq!(char_bumper.peek(), Some('a'));
+            assert_eq!(char_bumper.peek(), Some('a'));
         }
     }
 }
