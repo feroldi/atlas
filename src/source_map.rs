611ccd3880243e1cc26723ef3a03dd783d62fb68
Mário Feroldi <mferoldif@gmail.com>
2021-07-25 01:37:48 -0300

    Rename parse_start_pos_of_source_lines

--- src/source_map.rs
+++ src/source_map.rs
@@ -1,7 +1,7 @@
 #[derive(Debug, PartialEq)]
 struct BytePos(usize);
 
-fn parse_start_pos_of_source_lines(source_text: &str) -> Vec<BytePos> {
+fn calc_lines_start_positions(source_text: &str) -> Vec<BytePos> {
     if source_text.is_empty() {
         vec![]
     } else {
@@ -23,48 +23,48 @@ fn lookup_line_index(lines_start_pos: &[BytePos], pos: BytePos) -> Option<usize>
 
 #[cfg(test)]
 mod tests {
-    mod parse_start_pos_of_source_lines {
-        use crate::source_map::{parse_start_pos_of_source_lines, BytePos};
+    mod calc_lines_start_positions {
+        use crate::source_map::{calc_lines_start_positions, BytePos};
 
         #[test]
         fn empty_text() {
             let source_text = "";
-            let source_line_pos = parse_start_pos_of_source_lines(source_text);
+            let source_line_pos = calc_lines_start_positions(source_text);
             assert_eq!(source_line_pos, vec![]);
         }
 
         #[test]
         fn text_without_newline() {
             let source_text = "some text without newline";
-            let source_line_pos = parse_start_pos_of_source_lines(source_text);
+            let source_line_pos = calc_lines_start_positions(source_text);
             assert_eq!(source_line_pos, vec![BytePos(0usize)]);
         }
 
         #[test]
         fn text_with_newline_at_the_end() {
             let source_text = "abc\n";
-            let source_line_pos = parse_start_pos_of_source_lines(source_text);
+            let source_line_pos = calc_lines_start_positions(source_text);
             assert_eq!(source_line_pos, vec![BytePos(0usize)]);
         }
 
         #[test]
         fn text_with_newline_in_the_middle() {
             let source_text = "abc\ndef";
-            let source_line_pos = parse_start_pos_of_source_lines(source_text);
+            let source_line_pos = calc_lines_start_positions(source_text);
             assert_eq!(source_line_pos, vec![BytePos(0usize), BytePos(4usize)]);
         }
 
         #[test]
         fn text_with_newline_at_the_start() {
             let source_text = "\nabc";
-            let source_line_pos = parse_start_pos_of_source_lines(source_text);
+            let source_line_pos = calc_lines_start_positions(source_text);
             assert_eq!(source_line_pos, vec![BytePos(0usize), BytePos(1usize)]);
         }
 
         #[test]
         fn text_with_various_newlines_at_the_start() {
             let source_text = "\n\n\nabc";
-            let source_line_pos = parse_start_pos_of_source_lines(source_text);
+            let source_line_pos = calc_lines_start_positions(source_text);
             assert_eq!(
                 source_line_pos,
                 vec![
@@ -79,7 +79,7 @@ mod tests {
         #[test]
         fn text_with_various_newlines_at_the_end() {
             let source_text = "abc\n\n\n";
-            let source_line_pos = parse_start_pos_of_source_lines(source_text);
+            let source_line_pos = calc_lines_start_positions(source_text);
             assert_eq!(
                 source_line_pos,
                 vec![BytePos(0usize), BytePos(4usize), BytePos(5usize)]
